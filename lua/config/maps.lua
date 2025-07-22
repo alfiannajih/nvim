4115f9ccae183821e941320e63e8b24533a6a3f3
alfiannajih <alfiannajih@gmail.com>
2025-07-22 16:29:51 +0700

    feat: add description for mapped keys

--- lua/config/maps.lua
+++ lua/config/maps.lua
@@ -1,34 +1,32 @@
 vim.g.mapleader = " "
 
-local function map(mode, lhs, rhs)
-	vim.keymap.set(mode, lhs, rhs, { silent = true })
+local function map(mode, lhs, rhs, desc)
+	vim.keymap.set(mode, lhs, rhs, { noremap = true, silent = true, desc = desc })
 end
 
 -- Save
-map("n", "<leader>w", "<CMD>update<CR>")
+map("n", "<leader>w", "<CMD>update<CR>", "Save current file")
 
 -- Quit
-map("n", "<leader>q", "<CMD>q<CR>")
-
--- Exit insert mode
-map("i", "jk", "<ESC>")
+map("n", "<leader>q", "<CMD>q<CR>", "Close window")
 
 -- NeoTree
-map("n", "<leader>e", "<CMD>Neotree toggle<CR>")
-map("n", "<leader>r", "<CMD>Neotree focus<CR>")
-
 -- New Windows
 map("n", "<leader>o", "<CMD>vsplit<CR>")
 map("n", "<leader>p", "<CMD>split<CR>")
+map("n", "<leader>e", "<CMD>Neotree toggle<CR>", "Toggle explorer")
+map("n", "<leader>r", "<CMD>Neotree focus<CR>", "Switch to explorer")
 
 -- Window Navigation
-map("n", "<C-h>", "<C-w>h")
-map("n", "<C-l>", "<C-w>l")
-map("n", "<C-k>", "<C-w>k")
-map("n", "<C-j>", "<C-w>j")
+map("n", "<C-h>", "<C-w>h", "Move cursor to left window")
+map("n", "<C-l>", "<C-w>l", "Move cursor to right window")
+map("n", "<C-k>", "<C-w>k", "Move cursor to upper window")
+map("n", "<C-j>", "<C-w>j", "Move cursor to lower window")
 
 -- Resize Windows
-map("n", "<C-Left>", "<C-w><")
-map("n", "<C-Right>", "<C-w>>")
-map("n", "<C-Up>", "<C-w>+")
-map("n", "<C-Down>", "<C-w>-")
+map("n", "<C-Left>", "<C-w><", "Resize window to the left")
+map("n", "<C-Right>", "<C-w>>", "Resize window to the right")
+map("n", "<C-Up>", "<C-w>+", "Resize window to the top")
+map("n", "<C-Down>", "<C-w>-", "Resize window to the bottom")
+
+map("n", "g]", vim.diagnostic.goto_next, "Go to next diagnostic")
