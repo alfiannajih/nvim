6e5860f5a4fbe83837b13a6515a2b8ab1a72ffd2
alfiannajih <alfiannajih@gmail.com>
2025-07-22 16:34:02 +0700

    feat: add lsp_signature plugin

---
+++ lua/plugins/lsp_signature.lua
@@ -0,0 +1,13 @@
+return {
+	"ray-x/lsp_signature.nvim",
+	event = "InsertEnter",
+	opts = {
+		floating_window = true,
+		hint_enable = false,
+		always_trigger = true,
+		bind = true,
+		handler_opts = {
+			border = "rounded",
+		},
+	},
+}
